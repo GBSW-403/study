6563133afd617d19748ce6a06eb479b32c8647fb
hwnsng <amc214677@gmail.com>
2024-03-29 14:10:17 +0900

    first commit

---
+++ node_study2/index.js
@@ -0,0 +1,7 @@
+// DON'T TOUCH THIS LINE!
+const message = "    TASTE THE RAINBOW!  ";
+//DON'T CHANGE THIS LINE PLEASE! 
+console.log(message)
+var whisper = message.trim().toLowerCase()
+console.log(whisper)
+
